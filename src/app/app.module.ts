4ede67a18a4447c920837032bfc890f2318417cd
Jolien Foets <57670042+JolienFoets@users.noreply.github.com>
2021-09-25 10:43:22 +0200

    Les 2 - How to navigate between components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ArticleComponent } from './article/article.component';
+import { HomeComponent } from './home/home.component';
+import { MenuComponent } from './menu/menu.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ArticleComponent
+    ArticleComponent,
+    HomeComponent,
+    MenuComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
